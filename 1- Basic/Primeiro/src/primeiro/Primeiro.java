aee6312f73cec9a917ff8648b6f873b02d008de2
Augs Machado <augustomachadoramos@gmail.com>
2020-07-21 15:09:08 -0300

    Create Basic folder

---
+++ 1- Basic/Primeiro/src/primeiro/Primeiro.java
@@ -0,0 +1,59 @@
+package primeiro;
+
+/**
+ *
+ * @author Augs Machado
+ */
+public class Primeiro {
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro programa em Java");
+        
+        // 1. Frase na tela
+        System.out.println("O primeiro programa a gente nunca esquece");
+        
+        // 2. Etiqueta
+        System.out.print("Augs Machado\nRua das Camélias, 124\nCEP: 13065-147\n"
+                + "Tel: 019-98765-4321\n");
+        
+        // 3. Frases assassinas: são aquelas que muitas ideias se perdam ou não se aprofundem
+        System.out.println("Isso não vai dar certo");
+        System.out.println("Você nunca vai conseguir");
+        System.out.println("Você vai se estrepar");
+        System.out.println("Não vai dar em nada");
+        System.out.println("Está tudo errado");
+        
+        // 4. Mensagem
+        System.out.print("-> Não dê ouvidos a todos, somente preste aos atos dessa pessoa\n"
+                + "e veja se vive de acordo com o que te disse. Pq para dizer não faça,\n"
+                + "ela tem de ter feito primeiro.\n");
+        
+        // 5. Ao mestre
+        System.out.println("Obrigado por confiar em mim! Eu darei o meu melhor.");
+        
+        // 6. Quadrado
+        System.out.print("XXXXX\n"+"X   X\n"+"X   X\n"+"X   X\n"+"XXXXX\n");
+        
+        // 7. Tabela de notas
+        System.out.println("ALUNO(A)    NOTA");
+        System.out.println("========    ====");
+        System.out.println("Aline       9.0");
+        System.out.println("Mário       DEZ");
+        System.out.println("Sérgio      4.5");
+        System.out.println("Shirley     7.0");
+        
+        // 8. Letra grande
+        System.out.print(" JJJJJ\n"+"   J  \n"+"   J  \n"+"J  J   \n"+"JJJJ   \n");
+        
+        // 9. Menu
+        System.out.print("0- Fim\n1- Inclui\n2- Altera\n3- Exclui\n4- Consulta\nOpção:\n");
+        
+        // 10. Pinheiro
+        System.out.println("  X  ");
+        System.out.println(" XXX ");
+        System.out.println("XXXXX");
+        System.out.println("  X  ");
+        
+    }
+
+}
---
+++ 1- Basic/Teste/src/teste/Teste.java
@@ -0,0 +1,14 @@
+package teste;
+
+/**
+ *
+ * @author augus
+ */
+public class Teste {
+
+    // Olá, o que acontece se eu escrever isso?
+    public static void main(String[] args) {
+        System.out.println("Não sei! O que acontece se escrever aquilo lá?");
+    }
+    
+}
